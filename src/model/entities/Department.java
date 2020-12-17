e2a7955e00ac57769587111f78a579d33ad02000
Calebe Rodrigues <calebebrodrigues@gmail.com>
2020-12-16 22:26:08 -0300

    Department - hashCode and equals

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -26,6 +26,28 @@ public class Department {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 	
 	
 }
