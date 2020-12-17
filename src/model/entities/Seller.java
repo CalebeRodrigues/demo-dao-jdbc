82635b4355465451e2861a5d5683dcc9897fd674
Calebe Rodrigues <calebebrodrigues@gmail.com>
2020-12-17 18:19:17 -0300

    Seller - hashCode and equals

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -70,5 +70,27 @@ public class Seller {
 	public void setDeparment(Department deparment) {
 		this.deparment = deparment;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 	
 }
