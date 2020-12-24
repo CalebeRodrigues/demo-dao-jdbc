ccf15299f8b5ca931d96a63f65f44847fc8aa2f4
Calebe Rodrigues <calebebrodrigues@gmail.com>
2020-12-23 21:51:21 -0300

    Update test

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "Yasmin Ferreira", "yas29@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id = "+ newSeller.getId());
+		
+		System.out.println("\n\n---------- Test 5: Seller update ----------");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 
 }
